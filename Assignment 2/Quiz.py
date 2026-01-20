0de6c603b4ea51c1e4f630d8d2d50b19fbf269bc
w3s3 <raiyyan@hotmail.co.uk>
2026-01-20 03:39:19 +0000

    .

---
+++ Assignment 2/Quiz.py
@@ -0,0 +1,35 @@
+# Console based Quiz
+
+# A list of question, options and the correct answer
+quiz = [
+        {
+            # Question 1:
+            "question": "What company is PlayStation from?",
+            "options": ["Microsoft", "Sony", "Meta", "Dodge"],
+            "answer": "Sony"
+        },
+        {
+            # Question 2:
+            "question": "What game won game of the year 2022?",
+            "options": ["GOW Ragnarok", "Elden Ring", "Stray", "Horizon: Forbidden West"],
+            "answer": "Elden Ring"
+        },
+        {
+            # Question 3:
+            "question": "Who is the best chess player in the world?",
+            "options": ["Anatoly Karpov", "Hikaru Nakamura", "Magnus Carlson", "Oppenheimer"],
+            "answer": "Magnus Carlson"
+        },
+        {
+            # Question 4:
+            "question": "Which of one these Companies is German?",
+            "options": ["Siemens", "Gymshark", "Meta", "Ubisoft"],
+            "answer": "Siemens"
+        },
+        {
+            # Question 5:
+            "question": "Which one of these cities is located in the Middle East?",
+            "options": ["Sao Paulo", "Bangkok", "Manchester", "Sharjah"],
+            "answer": "Sharjah"
+        }
+        ]
